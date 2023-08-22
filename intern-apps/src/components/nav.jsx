a9544967f2fe7888ed3a63acb303ac94ad5bf38d
Muhammad Rifki Hidayatullah <rifki.hdyth@gmail.com>
2023-08-22 22:18:32 +0700

    salah teknis maap udah malem salah copas navbar

--- intern-apps/src/components/nav.jsx
+++ intern-apps/src/components/nav.jsx
@@ -1,155 +1,28 @@
-import Nav from "../components/nav"
-import sort from "../assets/sort.png"
+import user from '../assets/user.png'
 
-export default function Manager() {
-
-    const productsData = [
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-        {
-            judul: "Indomie goreng jumbo",
-            harga: "Rp.10.000/box",
-            stok: "100box"
-        },
-
-    ]
-
+export default function Nav() {
     return (
-        <div className="gradient min-h-screen">
-            <Nav />
-            <div className="px-8 mt-0 sm:mt-2.5">
-                <div className="flex justify-center">
-                    <div className="form-control flex-row items-center w-3/4 sm:w-auto">
-                        <div className="input-group justify-center">
-                            <input type="text" placeholder="Search product…" className="input input-bordered product-card input-sm sm:input-md sm:w-80 w-full sm:text-lg text-sm" />
-                            <button className="btn btn-square btn-sm sm:btn-md search-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-6 sm:w-6 search-img" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                            </button>
-                        </div>
-
-                    </div>
-                    <div className="dropdown ml-2 sm:ml-4 dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <img src={sort} className="h-8 sm:h-10" />
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow profile-drop rounded-box w-40">
-                            <li><a className="justify-center filters">Makanan</a></li>
-                            <li><a className="justify-center filters">Minuman</a></li>
-                            <li><a className="justify-center filters">Kesehatan</a></li>
-                            <li><a className="justify-center filters">Kecantikan</a></li>
-                            <li><a className="justify-center filters">Perabotan</a></li>
-                        </ul>
-                    </div>
-                </div>
+        <div className="navbar bg-transparent px-3 sm:px-7 py-6 sm:py-7 ">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-white normal-case text-2xl sm:text-[35px]">Industrial</a>
             </div>
-
-            <div className="w-full flex mt-12 sm:mt-20">
-                <div className="flex w-full justify-center">
-                    <div className="flex w-4/5 lg:w-3/5 justify-center flex-col shadow-bot">
-
-                        <div className="flex gap-4 sm:gap-5 flex-wrap justify-center h-[450px] overflow-y-scroll">
-                            {productsData.map((e, i) => (
-                                <div key={i} className="card w-44 h-24 sm:w-60 sm:h-28 bg-base-100 shadow-xl product-card rounded-[10px]">
-                                    <div className="card-body px-4 py-[15px]">
-                                        <h2 className="card-title text-xs sm:text-sm pt-0">{e.judul}</h2>
-                                        <p className="text-xs">Harga : {e.harga}</p>
-                                        <div className="flex flex-row">
-                                            <p className="text-xs stok">Tersedia : {e.stok}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
+            <div className="flex-none">
+                <div className="dropdown dropdown-end mr-1.5 sm:mr-0">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-9 sm:w-12 rounded-full">
+                            <img src={user} />
                         </div>
-                        <div className="divide mt-7"></div>
-                    </div>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-40 sm:w-48 items-center">
+                        <p
+                            className="justify-center font-bold text-lg mb-4 mt-3">
+                            Rifki
+
+                        </p>
+                        <p className="justify-center font-medium text-sm">Status : Manager</p>
+                        <p className="justify-center font-medium text-sm mt-2.5">08037362736</p>
+                        <button className="btn btn-sm logout text-white !text-xs w-28 mt-5 mb-3">Logout</button>
+                    </ul>
                 </div>
             </div>
         </div>
